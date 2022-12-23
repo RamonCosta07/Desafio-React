61b21b486d7639948dc810d8d0f0b28ece7fbe86
RamonCosta07 <ramonnar20@hotmail.com>
2022-12-23 01:48:42 -0300

    Desafio resolvido, utilizando hooks do react para solucionar as propostas

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { login } from "./utils";
+import "./index.css";
+import { useEffect, useState } from "react";
+
+// Instruções:
+// * Você tem um formulário de login INCOMPLETO
+// * Não é permitido adicionar novos elementos HTML
+// * Não é permitido usar refs
+//
+// Tarefas:
+// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
+// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
+// todo - Desabilite o botão de Login equanto você está executando o login.
+// todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
+// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const handleLogin = () => {
+    // e.preventDefault;
+    setLoading(true);
+    login({ email, password })
+      .then(() => {
+        window.alert(`Parabéns por conseguir logar`);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const load = () => {
+    setError(null);
+    if (email && password.length >= 6) {
+      setLoading(false);
+      return;
+    }
+    setLoading(true);
+  };
+
+  useEffect(() => {
+    load();
+  }, [email, password]);
+
+  return (
+    <div className="wrapper">
+      <div className="login-form">
+        <h1>Login Form 🐞</h1>
+        {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
+        {error && <div className="errorMessage">{error}</div>}
+        <div className="row">
+          <label htmlFor={"email"}>Email</label>
+          <input
+            id={"email"}
+            type={"email"}
+            autoComplete="off"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="row">
+          <label htmlFor={"password"}>Password</label>
+          <input
+            id={"password"}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type={"password"}
+          />
+        </div>
+
+        <div className="button">
+          {loading && (
+            <button onClick={handleLogin} disabled>
+              Login
+            </button>
+          )}
+          {!loading && <button onClick={handleLogin}>Login</button>}
+        </div>
+      </div>
+    </div>
+  );
+}
